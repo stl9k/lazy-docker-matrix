44d817ba1f64097add2598c3ed2aa299a37de404
stl9k <karkasik@yandex.ru>
2026-04-16 00:33:18 +0300

    Disabled registration

--- scripts/configure_synapse.sh
+++ scripts/configure_synapse.sh
@@ -32,9 +32,7 @@ echo ">>> Setting core configuration values..."
 
 # Set essential values
 set_yaml_value "public_baseurl" "\"https://${DOMAIN}\""
-set_yaml_value "enable_registration" "true"
-set_yaml_value "enable_registration_without_verification" "true"
-set_yaml_value "suppress_key_server_warning" "true"
+set_yaml_value "enable_registration" "false"
 
 # Update existing listeners to have x_forwarded: true
 sed -i '/listeners:/,/^[a-z]/{s/x_forwarded: false/x_forwarded: true/}' "${CONFIG_FILE}"
